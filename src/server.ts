bf4fce502fb1352b00d116b99c0af259146c3a48
Jefferson Brandao <jeffersonbraster@gmail.com>
2022-05-06 14:10:59 -0300

    port

--- src/server.ts
+++ src/server.ts
@@ -8,6 +8,6 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT || 80, () => {
   console.log("HTTP server running on port 3333");
 });
